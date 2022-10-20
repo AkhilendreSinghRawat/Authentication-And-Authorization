164cf8953e2bf171d1d049391a4525499ce72b02
Akhilendre singh rawat <akhilendre.rawat@daffodilsw.com>
2022-10-20 19:35:19 +0530

    setup

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,17 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import SignUp from './SignUp'
 
 const App = () => {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<SignUp />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App
